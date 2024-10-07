a9868ac70a6ff927ef8e4da27c60ab7afeb07dec
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-07 17:00:10 -0400

    // display account balance monthly for 12 months

---
+++ forLoopAccountBalance.js
@@ -0,0 +1,8 @@
+// display account balance monthly for 12 months
+
+let monthlyDeposit = 100; // Code will be tested with values: 100 and 130
+let accountBalance = 0;
+
+for (let i = 1; i <= 12; i++) {
+  console.log(accountBalance + monthlyDeposit * i);
+}
