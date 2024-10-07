3eac40d7b9b6ba77240bde0c0298d8a4c9c580c3
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-07 17:00:13 -0400

    // create a function expression (function assigned to variable)

---
+++ functionExpression.js
@@ -0,0 +1,9 @@
+// create a function expression (function assigned to variable)
+
+const functionExpression = function (x, y, z) {
+  return z - y + 2 * x;
+};
+
+// or
+
+const arrowFunction = (x, y, z) => z - y + 2 * x;
