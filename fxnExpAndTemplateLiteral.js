925a3850671faac2c1ea054b45928b2140569bff
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-08 17:00:01 -0400

    // create a matlib function expression

---
+++ fxnExpAndTemplateLiteral.js
@@ -0,0 +1,7 @@
+// create a matlib function expression
+
+const madLib = (adv, n1, n2, adj, v) =>
+  `The ${adj} ${n1} ${adv} ${v} the ${n2}`;
+
+console.log(madLib('quietly', 'dog', 'moon', 'lazy', 'smashed'));
+// The lazy dog quietly smashed the moon
