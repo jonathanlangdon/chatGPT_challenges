50db4cda8cedc4eb3fddf61e4098802c212b0806
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-08 17:00:04 -0400

    // display the name that is alphabetically first

---
+++ orderNamesAndDisplay.js
@@ -0,0 +1,7 @@
+// display the name that is alphabetically first
+
+let userName1 = 'Ann';
+let userName2 = 'Anthony';
+
+const nameOrder = [userName1, userName2].sort();
+console.log(nameOrder[0]);
