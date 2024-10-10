088dea2f5174df24f1fcc083d97f6333438ad5b0
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-10 17:00:02 -0400

    // App.js for Forms & Modals

---
+++ reactJSFormsAndModals-App.js
@@ -0,0 +1,16 @@
+// App.js for Forms & Modals
+
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <Container>
+      <h1>Loan Calculator</h1>
+      <Calculator />
+    </Container>
+  );
+}
+
+export default App;
