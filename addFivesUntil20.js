96e6408b360d0bf74473a5ae3954c6c7773db374
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-11 17:00:07 -0400

    // Write a while loop that adds 5 to userNum while userNum is less than 20, displaying the result after each addition.

---
+++ addFivesUntil20.js
@@ -0,0 +1,8 @@
+// Write a while loop that adds 5 to userNum while userNum is less than 20, displaying the result after each addition.
+
+let userNum = 5;
+
+while (userNum < 20) {
+  userNum += 5;
+  console.log(userNum);
+}
