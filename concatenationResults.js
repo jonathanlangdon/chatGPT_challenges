aa13c7ea3016d9a44eb63a246da2ee9e03aea6a4
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-12 17:00:09 -0400

    // concatenate and add values

---
+++ concatenationResults.js
@@ -0,0 +1,10 @@
+// concatenate and add values
+
+let monthName = 'May ';
+let dayNumber = '4';
+let valueNumber = 7;
+let userDate = monthName + dayNumber;
+let numberResult = dayNumber + valueNumber;
+
+console.log(userDate); // May 4
+console.log(numberResult); // 47
