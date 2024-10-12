fb4eb71157cb305fde3a49b0df030a1406516899
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-12 17:00:01 -0400

    // Compare the given strings and display the string that comes alphabetically last.

---
+++ alphabeticallyLastName.js
@@ -0,0 +1,6 @@
+// Compare the given strings and display the string that comes alphabetically last.
+
+let friendName1 = 'Ann';
+let friendName2 = 'Anthony';
+
+console.log([friendName2, friendName1].sort()[1]);
