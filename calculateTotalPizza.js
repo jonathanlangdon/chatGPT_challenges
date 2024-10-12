2914e83ca7ea42197a393a4c64da70022df2b328
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-12 17:00:07 -0400

    // compute the total number of pizzas needed to feed all the guests

---
+++ calculateTotalPizza.js
@@ -0,0 +1,7 @@
+// compute the total number of pizzas needed to feed all the guests
+let slicesPerPizza = 8;
+let slicesPerGuest = 4;
+let totalPersons = 12;
+let totalPizzas = (totalPersons * slicesPerGuest) / slicesPerPizza;
+
+console.log(totalPizzas); // 6
