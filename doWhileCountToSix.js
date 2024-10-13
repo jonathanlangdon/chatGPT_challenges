55afebc7813efef080567edf4bbbcda6fd515e45
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-13 17:00:07 -0400

    // Write a do-while loop that counts up from numVar to 6. Ex: For numVar = 3, output is: 3 4 5 6 (with each value output on a separate line)

---
+++ doWhileCountToSix.js
@@ -0,0 +1,8 @@
+// Write a do-while loop that counts up from numVar to 6. Ex: For numVar = 3, output is: 3 4 5 6 (with each value output on a separate line)
+
+let numVar = 3;
+
+do {
+  console.log(numVar);
+  numVar += 1;
+} while (numVar <= 6);
