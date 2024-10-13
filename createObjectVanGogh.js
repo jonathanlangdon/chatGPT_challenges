122a6a3ee9117cc1051e71281d41e0c8e98472d2
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-13 17:00:01 -0400

    // create new object with properties about VanGogh

---
+++ createObjectVanGogh.js
@@ -0,0 +1,8 @@
+// create new object with properties about VanGogh
+
+let painter = {
+  name: 'Vincent van Gogh',
+  artworks: ['The Starry Night', 'Irises', 'The Potato Eaters'],
+  bornDate: 'March 30, 1853',
+  diedDate: 'July 29, 1890'
+};
