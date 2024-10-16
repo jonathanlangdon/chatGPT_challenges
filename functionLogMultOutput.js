61f2b7b924739297cb20bf59facfb498dad1b12f
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-16 17:00:10 -0400

    // Declare and define a function called displayMult that uses console.log() to display the result of multiplying two parameters.

---
+++ functionLogMultOutput.js
@@ -0,0 +1,5 @@
+// Declare and define a function called displayMult that uses console.log() to display the result of multiplying two parameters.
+
+const displayMult = (n1, n2) => console.log(n1 * n2);
+
+displayMult(3, 4);
