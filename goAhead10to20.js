cd01df0d7211c42f399a773eea722e640c74be40
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-17 17:00:12 -0400

    // Write an if statement that displays "Go ahead!" if userAge is between 10 and 20 inclusive.

---
+++ goAhead10to20.js
@@ -0,0 +1,4 @@
+// Write an if statement that displays "Go ahead!" if userAge is between 10 and 20 inclusive.
+
+let userAge = 12;
+if (userAge >= 10 && userAge <= 20) console.log('Go ahead!'); // Go ahead!
