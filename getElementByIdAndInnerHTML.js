c3ee64843ff42a4f69753da18a20719ff163ee9e
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-17 17:00:07 -0400

    //assign the h6 element with id inspiring-quote the HTML "<u>Whatever you do, do it well.</u>".

---
+++ getElementByIdAndInnerHTML.js
@@ -0,0 +1,4 @@
+//assign the h6 element with id inspiring-quote the HTML "<u>Whatever you do, do it well.</u>".
+
+document.getElementById('inspiring-quote').innerHTML =
+  '<u>Whatever you do, do it well.</u>';
