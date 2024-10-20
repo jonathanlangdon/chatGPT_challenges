086503ab5e2f25640aafd5c13c7e145b8641358b
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-20 17:01:05 -0400

    //set the paragraph's inline style to use the 10px font size and remove the paragraph's background color property.

---
+++ modifyCSSbackgroundfont.js
@@ -0,0 +1,6 @@
+//set the paragraph's inline style to use the 10px font size and remove the paragraph's background color property.
+
+const helloElem = document.querySelector('#helloMessage');
+
+helloElem.style.fontSize = '10px';
+helloElem.removeProperty('background');
