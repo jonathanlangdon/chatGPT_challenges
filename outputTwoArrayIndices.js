f7a59877d3e82d94e7925fba9a3ca8dcaf06876a
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-21 17:01:07 -0400

    //Output the elements at indices 1 and 4 in the array userNumbers separated by spaces.

---
+++ outputTwoArrayIndices.js
@@ -0,0 +1,4 @@
+//Output the elements at indices 1 and 4 in the array userNumbers separated by spaces.
+
+let userNumbers = [1, 6, 41, 8, 24, 4]; // Tests may use different array values
+console.log(userNumbers[1] + ' ' + userNumbers[4]);
