def6f2bcaddb9ee5065b9e723fb176bd6071719d
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-10-26 17:01:02 -0400

    // Prompt the user, asking "What city do you live in?", then display the result as: Fall in ____ is pleasant.

---
+++ promptUserForCity.js
@@ -0,0 +1,4 @@
+// Prompt the user, asking "What city do you live in?", then display the result as: Fall in ____ is pleasant.
+
+const city = prompt('What city do you live in?');
+console.log(`Fall in ${city} is pleasant.`);
