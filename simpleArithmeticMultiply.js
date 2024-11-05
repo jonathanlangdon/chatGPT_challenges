77c56247276bf013c26ec2f9e4f1429a77df7dda
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-11-05 17:01:10 -0500

    // Write a statement that assigns finalResult with the multiplication of number1 and number2.

---
+++ simpleArithmeticMultiply.js
@@ -0,0 +1,7 @@
+// Write a statement that assigns finalResult with the multiplication of number1 and number2.
+
+let number1 = 6;
+let number2 = 2;
+let finalResult = number1 * number2;
+
+console.log(finalResult);
