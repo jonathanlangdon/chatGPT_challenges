a1ccf39f37f8316068f401ef7d340b48479e9dfe
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-11-07 17:01:10 -0500

    // construct a simple while loop

---
+++ simpleWhileLoop.js
@@ -0,0 +1,9 @@
+// construct a simple while loop
+
+let enteredNum = 40; // Code will be tested with values: 40, 4 and -5
+
+/* Your solution goes here */
+while (enteredNum > 1) {
+  enteredNum = enteredNum / 4;
+  console.log(enteredNum);
+}
