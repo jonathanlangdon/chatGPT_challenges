0c005d88d3b923633dda93d44cc956501591426d
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-11-11 17:01:07 -0500

    // Write a switch statement that examines inputItem. If 0, print "HTML". If 1, print "CSS". If 2, print "JavaScript". For any other value, print "PHP".

---
+++ switchStatementCodeLanguage.js
@@ -0,0 +1,19 @@
+// Write a switch statement that examines inputItem. If 0, print "HTML". If 1, print "CSS". If 2, print "JavaScript". For any other value, print "PHP".
+
+let inputItem = 0;
+
+switch (inputItem) {
+  case 0:
+    console.log('HTML');
+    break;
+  case 1:
+    console.log('CSS');
+    break;
+  case 2:
+    console.log('JavaScript');
+    break;
+  default:
+    console.log('PHP');
+}
+
+// HTML
