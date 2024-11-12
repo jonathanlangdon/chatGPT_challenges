d7dcdc9b7aa00ee74617ea27a10a711e03663b2b
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-11-12 17:01:13 -0500

    // Complete the conditional operator so ticketPrice is assigned with 11 if membershipLevel is 7 or above, or with 19 otherwise

---
+++ ticketPriceMembership.js
@@ -0,0 +1,6 @@
+// Complete the conditional operator so ticketPrice is assigned with 11 if membershipLevel is 7 or above, or with 19 otherwise
+
+let membershipLevel = 8;
+let ticketPrice = membershipLevel >= 7 ? 11 : 19;
+
+console.log(ticketPrice); // 11
