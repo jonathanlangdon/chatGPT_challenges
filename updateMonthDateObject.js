b566efe667dcf1fa16e60de7c590409a112ec831
Jonathan Langdon <langdon.jonathan@gmail.com>
2024-11-14 17:01:09 -0500

    // Update the variable updatedOn to the month March using Date methods. Months are numbered from 0 to 11. January is month 0.

---
+++ updateMonthDateObject.js
@@ -0,0 +1,6 @@
+// Update the variable updatedOn to the month March using Date methods. Months are numbered from 0 to 11. January is month 0.
+
+let updatedOn = new Date(2010, 3, 21);
+updatedOn.setMonth(2);
+
+console.log(updatedOn);
